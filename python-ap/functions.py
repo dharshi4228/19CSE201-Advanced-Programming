5cbb3839788f513c47661205190d7a7c05eac802
DHARSHIKA S <116824635+dharshi4228@users.noreply.github.com>
2023-10-30 16:14:20 +0530

    30/10/2023

---
+++ python-ap/functions.py
@@ -0,0 +1,13 @@
+cys=['deepak','dharshika','hamsini']
+cys.insert(0,'hansica')
+print(cys)
+cys.append('vetrivel')
+print(cys)
+cys.pop(4)
+print(cys)
+
+cys.remove('hansica')
+print(cys)
+cys.clear()
+del cys
+print(cys)
